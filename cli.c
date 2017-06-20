54351737d0cdba797c3acaed05f97cfb71477e82
Heng Li <lh3@me.com>
2017-06-19 23:11:27 -0400

    repeating for benchmarking

--- cli.c
+++ cli.c
@@ -39,35 +39,40 @@ static void ksw_gen_simple_mat(int m, int8_t *mat, int8_t a, int8_t b)
 		mat[(m - 1) * m + j] = 0;
 }
 
-static void global_aln(const char *algo, void *km, char *qseq, char *tseq, int8_t m, const int8_t *mat, int8_t q, int8_t e, int w, int zdrop, int flag, ksw_extz_t *ez)
+static void global_aln(const char *algo, void *km, const char *qseq_, const char *tseq_, int8_t m, const int8_t *mat, int8_t q, int8_t e, int w, int zdrop, int flag, ksw_extz_t *ez)
 {
 	int i, qlen, tlen;
-	qlen = strlen(qseq);
-	tlen = strlen(tseq);
+	uint8_t *qseq, *tseq;
+	qlen = strlen(qseq_);
+	tlen = strlen(tseq_);
+	qseq = (uint8_t*)malloc(qlen);
+	tseq = (uint8_t*)malloc(tlen);
 	for (i = 0; i < qlen; ++i)
-		qseq[i] = seq_nt4_table[(uint8_t)qseq[i]];
+		qseq[i] = seq_nt4_table[(uint8_t)qseq_[i]];
 	for (i = 0; i < tlen; ++i)
-		tseq[i] = seq_nt4_table[(uint8_t)tseq[i]];
+		tseq[i] = seq_nt4_table[(uint8_t)tseq_[i]];
 	if (w < 0) w = qlen > tlen? qlen : tlen;
 	if (strcmp(algo, "gg") == 0)           ez->score = ksw_gg(0, qlen, (uint8_t*)qseq, tlen, (uint8_t*)tseq, 5, mat, q, e, w, &ez->m_cigar, &ez->n_cigar, &ez->cigar);
 	else if (strcmp(algo, "gg2") == 0)     ez->score = ksw_gg2(0, qlen, (uint8_t*)qseq, tlen, (uint8_t*)tseq, 5, mat, q, e, w, &ez->m_cigar, &ez->n_cigar, &ez->cigar);
 	else if (strcmp(algo, "gg2_sse") == 0) ez->score = ksw_gg2_sse(0, qlen, (uint8_t*)qseq, tlen, (uint8_t*)tseq, 5, mat, q, e, w, &ez->m_cigar, &ez->n_cigar, &ez->cigar);
 	else if (strcmp(algo, "extz2_sse") == 0) ksw_extz2_sse(0, qlen, (uint8_t*)qseq, tlen, (uint8_t*)tseq, 5, mat, q, e, w, zdrop, flag, ez);
 	else abort();
+	free(qseq); free(tseq);
 }
 
 int main(int argc, char *argv[])
 {
 	int8_t a = 1, b = 1, q = 1, e = 1;
-	int c, i, pair = 1, w = -1, flag = KSW_EZ_SIMPLE_SC;
+	int c, i, pair = 1, w = -1, flag = KSW_EZ_SIMPLE_SC, rep = 1;
 	char *algo = "gg2";
 	int8_t mat[25];
 	ksw_extz_t ez;
 	gzFile fp[2];
 
-	while ((c = getopt(argc, argv, "t:w:S")) >= 0) {
+	while ((c = getopt(argc, argv, "t:w:R:")) >= 0) {
 		if (c == 't') algo = optarg;
 		else if (c == 'w') w = atoi(optarg);
+		else if (c == 'R') rep = atoi(optarg);
 	}
 	if (argc - optind < 2) {
 		fprintf(stderr, "Usage: ksw2-global <DNA-target> <DNA-query>\n");
@@ -91,7 +96,8 @@ int main(int argc, char *argv[])
 		if (pair) {
 			while (kseq_read(ks[0]) > 0) {
 				if (kseq_read(ks[1]) <= 0) break;
-				global_aln(algo, 0, ks[0]->seq.s, ks[1]->seq.s, 5, mat, q, e, w, 100, flag, &ez);
+				for (i = 0; i < rep; ++i)
+					global_aln(algo, 0, ks[0]->seq.s, ks[1]->seq.s, 5, mat, q, e, w, 100, flag, &ez);
 				printf("%s\t%s\t%d\t", ks[0]->name.s, ks[1]->name.s, ez.score);
 				for (i = 0; i < ez.n_cigar; ++i)
 					printf("%d%c", ez.cigar[i]>>4, "MID"[ez.cigar[i]&0xf]);
